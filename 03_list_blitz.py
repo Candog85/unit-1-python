f84cb86dcfc7589c0abe374ec3ab880c3df6fbad
candog85 <candog851@gmail.com>
2024-09-19 10:50:15 -0400

    Started lesson 3

---
+++ 03_list_blitz.py
@@ -0,0 +1,79 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+
+global list_var
+
+#Creates a list
+list_var=["item 1", "item 2", "item 3", "item 4"]
+
+#prints said list
+print(list_var) 
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+
+#Appends item 5 prints updated
+list_var.append("item 5")
+print(list_var)
+
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+
+#Deletes first item, prints updated list
+list_var.remove("item 1")
+print(list_var)
+
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+
+#Replaces item index 1 in the list
+list_var[1]="replaced item"
+print(list_var)
+
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+
+#Appends one item, then the next
+list_var.append("multi append 1")
+list_var.append("multi append 2")
+
+#Prints updated list
+print(list_var)
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+
+#Deletes the item of index 1 and pronts the updated list
+del list_var[2]
+print(list_var)
+
+"""
+Task 7: Subsetting lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+
+new_list=(list_var[1]+)
+
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
