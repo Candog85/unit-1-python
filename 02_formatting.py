f5358383c4cd3cf82f66d18208ce22e77b97aae7
Candog85 <candog851@gmail.com>
2024-09-18 22:32:21 -0400

    Finished lesson 2

--- 02_formatting.py
+++ 02_formatting.py
@@ -7,7 +7,7 @@ The password should not be case sensitive
 def task_1():
 
     #Takes user password input, makes it lowercase and strips it
-    attempt=(input("Enter Password: ")).lower()
+    attempt=input("Enter Password: ").lower().strip()
 
     #Real password
     password="fujin"
@@ -49,10 +49,10 @@ It should replace all occurances regardless of captilization
 
 def task_3():
 
-    #Defines a phrase
+    #Defines a string phrase
     phrase="I love Cats and CATS are so cool and cats are awesome!"
 
-    #Checks if the lowercased phrase has 
+    #Makes the string lowercase. Checks if it has "cats" and replaces with "dogs"
     phrase=(phrase.lower()).replace("cats","dogs")
     print(phrase)
 
@@ -63,6 +63,16 @@ TASK 4:
 
 Write a program that takes a person's name and age as input and prints it
 """
+def task_4():
+
+    #Asks user for name and age amd strips
+    name=input("What is your name?: ").strip()
+    age=int(input("What is your age?: ").strip())
+
+    #Uses f string to insert name and age values into string
+    print(f"Your name is {name}, and you are {age} years old!")
+
+task_4()
 
 
 """
@@ -71,3 +81,14 @@ TASK 5:
 Write a program that takes in two floats, and prints the quotient
 The result should be rounded to the nearest tenth (1 decimal place)
 """
+
+def task_5():
+
+    #Takes input for two floats and makes them floats
+    number_1=float(input("Enter number 1: ").strip())
+    number_2=float(input("Enter number 2: ").strip())
+
+    #Prints string with placeholder, first number divided by second, and rounded using format specifier ".1f"
+    print(f"The rounded quotient is {number_1/number_2:.1f}")
+
+task_5()
