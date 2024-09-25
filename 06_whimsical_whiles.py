2e2b1173c64177cd40585d657eaf07521519097c
candog85 <candog851@gmail.com>
2024-09-25 10:51:54 -0400

    Started seuff idk bro

--- 06_whimsical_whiles.py
+++ 06_whimsical_whiles.py
@@ -2,14 +2,16 @@
 1. Simple Counter:
 Write a program that uses a while loop to print numbers from 1 to 10.
 """
-
 def task_1():
 
-    counter=1
+    #Placehoder for counter
+    counter=0
 
-    while counter<=10:
-        print(counter)
+    #While counter is less than 1, add one to counter and print its value
+    while counter<10:
         counter+=1
+        print(counter)
+        
 
 task_1()
 
@@ -20,8 +22,10 @@ Write a program that uses a while loop to print numbers from 10 to 1 in descendi
 
 def task_2():
 
+    #Placeholder for counter, starts 10
     counter=10
 
+    #While counter is greater than or equal to 1, print its value
     while counter>=1:
         print(counter)
         counter-=1
@@ -33,18 +37,27 @@ task_2()
 Write a program that calculates the factorial of a given number using a while loop.
 """
 
-def task_2():
+def task_3():
 
+    #Takes user input for the number being used
     number=float(input("Enter a number: "))
+
+    #Starts at ten
     counter=10
 
+    #While counter is a positive integer, 
     while counter>=1:
+
+        #number is multiplied by counter
         number=number*counter
+
+        #counter is decreased by one
         counter-=1
 
+    #Prints result
     print(number)
 
-task_2()
+task_3()
 
 
 """
@@ -52,18 +65,27 @@ task_2()
 Create a simple password guessing game using a while loop. Ask the user to guess a predefined password and provide appropriate feedback.
 """
 
-def task_3():
+def task_4():
 
+    #Preset password "meggido"
     password="meggido"
+
+    #Takes input for the guess
     guess=input("Guess the password: ")
     
+    #If the guess isn't the password
     while guess!=password:
+
+        #Prints incorrect password
         print("Incorrect password!")
+
+        #Reprompt
         guess=input("Guess the password: ")
     
+    #When the loop is broken (guess=password), prints:
     print("Correct!")
 
-task_3()
+task_4()
 
 """
 5. Sum of Digits:
@@ -72,15 +94,34 @@ Write a program that calculates the sum of the digits of a given number using a 
 
 def task_5():
 
-    number=[int(input("Enter an integer: "))]
+    sum_var=0
 
-    for x in number:
-        print(x)
+    number=input("Enter an integer: ")
 
-task_5()
+    for digits in number:
+        sum_var=sum_var+int(digits)
+    
+    print(sum_var)
+
+#task_5()
 
 """
 6. Fibonacci Series:
 Write a program that prints the first n numbers in the Fibonacci sequence using a while loop.
 """
 
+def task_6():
+
+    #List of the first 2 fibbonachi numbers
+    fibonacci=[0,1]
+
+    #Takes input for the amount of numbers to be printed
+    n=int(input("How many fibbonachi numbers?"))
+
+    #For loop in range og 
+    for x in range(n):
+        fibonacci.append(fibonacci[-1]+fibonacci[-2])
+    
+    print(fibonacci)
+
+task_6()    
---
+++ 07_flipping_fors.py
@@ -0,0 +1,42 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+
+#Defines string
+string="Im so happy"
+
+#For each letter in string, print the letter
+for letters in string:
+    print(letters)
+
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+
+#Define list and placeholder for sum
+list_var=[1,2,3,4,5]
+sum_var=0
+
+#For each number in the list, add that number to the s
+for number in list_var:
+    sum_var=sum_var+number
+    print(sum_var)
+    
+
+"""
+Exercise 3:
+Write a program to print the length of each word in a sentence using a for loop and a list.
+"""
+
+
+
+"""
+Excercise 4:
+Write a program that creates a dictionary (atleast 4 key:value pairs) and then
+iterates through a dictionary and prints the result
+
+In a comment, answer the following, what do you notice about the output of your code?
+Is it what you expected?
+"""
