9d21f92af83a72c86ec3ca1f3d5ca2f25c751acb
Candog85 <candog851@gmail.com>
2024-09-25 23:43:12 -0400

    Finished lesson 7 (Flipping fors)

--- 07_flipping_fors.py
+++ 07_flipping_fors.py
@@ -19,7 +19,7 @@ Write a program to calculate the sum of elements in a list using a for loop.
 list_var=[1,2,3,4,5]
 sum_var=0
 
-#For each number in the list, add that number to the s
+#For each number in the list, add that number to the sum variable and print it
 for number in list_var:
     sum_var=sum_var+number
     print(sum_var)
@@ -30,6 +30,12 @@ Exercise 3:
 Write a program to print the length of each word in a sentence using a for loop and a list.
 """
 
+#Takes user input for sentence, splits it into list.
+sentence=input("Enter your sentence: ").split()
+
+#For each word in the sentence, print the length of the word
+for words in sentence:
+    print(len(words))
 
 
 """
@@ -40,3 +46,18 @@ iterates through a dictionary and prints the result
 In a comment, answer the following, what do you notice about the output of your code?
 Is it what you expected?
 """
+
+#Defines dictionary with 5 items    
+dictionary={
+    "Banana":"Fruit",
+    "Apple":"Fruit",
+    "Lettuce":"Vegetable",
+    "Tomato":"Fruit",
+    "Orange":"Fruit",
+}
+
+#Prints the key of each item in the dictionary
+for items in dictionary:
+    print(items)
+
+#I expected that the value of each key would be printed instead of the key
