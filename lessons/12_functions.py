68986b63da33ad5f5e9568734a2b829ca9c1abfb
Candog85 <candog851@gmail.com>
2024-10-09 20:40:41 -0400

    Finished 12

--- lessons/12_functions.py
+++ lessons/12_functions.py
@@ -4,7 +4,7 @@ Write a function that takes a name as a
 parameter and prints a greeting message like "Hello, [name]!".
 """
 
-#defines function as greeting, with name perameter
+#defines function as greeting, with name parameter
 def greeting(name):
     #prints "Hello (name)""
     print(f"Hello, {name}")
@@ -63,10 +63,14 @@ Write a function that takes a temperature in Celsius and returns
 the equivalent temperature in Fahrenheit using the correct formula
 """
 
+#defines function temp_convert with temp as parameter
 def temp_convert(temp):
+
+    #returns temp processed through the conversion formula for celsius to fahrenheit 
     return temp*1.8+32
 
-print(f"Your converted farenheit value is {temp_convert(54)} degrees")
+#prints result of temp_convert using 54* C as parameter
+print(f"Your converted fahrenheit value is {temp_convert(54)} degrees")
 
 """
 Task 6: Average of Numbers
@@ -74,12 +78,22 @@ Write a function that takes a list of numbers
 and returns the average (mean) of those numbers.
 """
 
+#defines function average with numbers as parameter
 def average(numbers):
+
+    #defines sum as empty variable
     sum=0
+
+    #for every number in the provided list of numbers using the "numbers" parameter:
     for number in numbers:
+
+        #add that number to the sum
         sum=sum+number
+    
+    #return the sum divided by the amount of numbers using the len function
     return sum/len(numbers)
 
+#print the result of the average function using 10, 23, 30, and 20 as list of numbers.
 print(f"The average of your list is {average([10, 23, 30, 20])}")
 
 """
@@ -88,11 +102,20 @@ Create a function that has arguments for the price and quantity of something, an
 Your function should also optionally accept a 3rd argument for discount, and return the discounted if provided.
 """
 
+#defines function as calculator with parameters price, quantity, and discount. Discount is 0 by default, in case there is no discount
 def calculator(price, quantity, discount=0):
 
     total=price*quantity
 
+    #converts discount parameter from whole percentage to decimal percentage
     discount=discount/100
     
+    #if discount is not 0, the total is reduced by the percentage of the discount
     if discount>0:
-        total=total-discount
+        total=total-(total*discount)
+    
+    #returns the total variable
+    return total
+
+#prints result of calculator using three $20 items and a 10% discount
+print(calculator(20, 3, 10))
